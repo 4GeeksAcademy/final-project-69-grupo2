00a30099e56bfa9325896972658354aa564d46f9
Marcos Cortez <bensirave@gmail.com>
2026-04-24 11:04:18 +0000

    listo el backend de usuario y registro

--- src/api/routes.py
+++ src/api/routes.py
@@ -9,7 +9,7 @@ from flask_cors import CORS
 from werkzeug.security import generate_password_hash, check_password_hash
 from base64 import b64encode
 import os
-from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity, JWTManager
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity, JWTManager, get_jwt, decode_token
 from datetime import timedelta
 
 api = Blueprint('api', __name__)
@@ -120,3 +120,114 @@ def example_email():
         return jsonify({"error": "Failed to send email"}), 500
 
 
+@api.route('/reset-password', methods=['POST'])
+def reset_password():
+    data = request.get_json()
+    email = data.get("email", "").strip().lower()
+
+    if not email:
+        return jsonify({"error": "Missing required field: email"}), 400
+
+    user = User.query.filter_by(email=email).one_or_none()
+    if not user:
+        return jsonify({"error": "If email exists, a password reset email will be sent"}), 404
+
+    reset_token = create_access_token(identity=str(
+        user.id), additional_claims={"purpose": "password-reset"}, expires_delta=timedelta(minutes=10))
+    frontend_url = os.getenv("URL_FRONTEND" or "").strip()
+
+    if not frontend_url:
+        return jsonify({"error": "Frontend URL is not configured"}), 500
+
+    reset_link = f"{frontend_url}reset-password?token={reset_token}"
+
+    subject = "Solicitud de restaurar la contraseña"
+
+    body = f"""
+        <div>
+            <p>Hola {user.username},</p>
+            <p>Solicitud para restaurar la contraseña. Da click en el siguiente enlace:</p>
+            <a href="{reset_link}">Reset Password</a>
+            <p>Si tu no solicitaste este enlace puedes ignorarlo.</p>
+        </div>
+    """
+
+    try:
+        success = send_email(
+            to=user.email,
+            subject=subject,
+            body=body
+        )
+        if success:
+            return jsonify({"message": "Email sending success"}), 200
+        else:
+            return jsonify({"error": "Error sended message"})
+    except Exception as error:
+        return jsonify({"error": f"Error sending email: {error.args}"})
+
+
+@api.route("/update-pwd", methods=["POST"])
+@jwt_required()
+def update_password():
+    claims = get_jwt()
+    if claims.get("purpose") != "password-reset":
+        return jsonify({"error": "Invalid tokoken for password update"}), 403
+
+    user_id = get_jwt_identity()
+    user = User.query.get(user_id)
+
+    if not user:
+        return jsonify({"error": "User not found"}), 404
+
+    data = request.get_json()
+    new_password = data.get("new_password", "")
+
+    if not new_password:
+        return jsonify({"error": "Misssing required field: new_password"}), 400
+
+    salt = b64encode(os.urandom(32)).decode("utf-8")
+    user.password = generate_password_hash(new_password+salt)
+    user.salt = salt
+
+    try:
+        db.session.commit()
+        return jsonify({"message": "password updated successfully"}), 200
+    except Exception as error:
+        db.session.rollback()
+        print(error.args)
+        return jsonify({"error": f"Error updating password: {error.args}"}), 500
+
+
+@api.route("/activate-account", methods=["POST"])
+def activate_account():
+    data = request.get_json(silent=True) or {}
+    token = (data.get("token") or request.args.get("token") or "").strip()
+
+    if not token:
+        return jsonify({"error": "Missing required field. token"}), 400
+
+    try:
+        decoded = decode_token(token)
+
+    except Exception as error:
+        return jsonify({"error": f"Invalid token or expired token: {error.args}"}), 400
+
+    if decoded.get("purpose") != "account_activation":
+        return jsonify({"error": "Ivalid token purpose"}), 403
+
+    user_id = decoded.get("sub")
+    user = User.query.get(user_id)
+    if not user:
+        return jsonify({"error": "user not found"}), 404
+
+    if user.is_active:
+        return jsonify({"message": "User already activated"}), 200
+
+    user.is_active = True
+
+    try:
+        db.session.commit()
+        return jsonify({"message": "User activated successfully"}), 200
+    except Exception as error:
+        db.session.rollback()
+        return jsonify({"error": f"Error activating user: {error.args}"}), 500
