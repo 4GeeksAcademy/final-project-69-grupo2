25f6205f692220dfc22ad12f430533b791c791dd
pantaleonyy <yona.pantaleon48@icloud.com>
2026-05-11 22:52:50 +0000

    feat-footer

--- src/front/pages/Layout.jsx
+++ src/front/pages/Layout.jsx
@@ -3,13 +3,16 @@ import ScrollToTop from "../components/ScrollToTop"
 import { Navbar } from "../components/Navbar"
 import { Footer } from "../components/Footer"
 
-// Base component that maintains the navbar and footer throughout the page and the scroll to top functionality.
 export const Layout = () => {
     return (
         <ScrollToTop>
-            <Navbar />
-                <Outlet />
-            <Footer />
+            <div style={{ display: "flex", flexDirection: "column", minHeight: "100vh" }}>
+                <Navbar />
+                <div style={{ flex: 1 }}>
+                    <Outlet />
+                </div>
+                <Footer />
+            </div>
         </ScrollToTop>
     )
 }
